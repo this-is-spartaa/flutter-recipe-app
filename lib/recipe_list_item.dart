a4e28204cbbf80f6c0815c4b77488d099811d856
mk <mk@mkui-MacBookPro.local>
2024-09-28 07:13:32 +0900

    add RecipeTitle

---
+++ lib/recipe_list_item.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class RecipeListItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text("RecipeListItem");
+  }
+}
